76cfa528bcde47f989a8d8b200fd9bc5cd828bd3
Chad Luo <Chad.Luo@walmart.com>
2018-04-03 14:05:25 +0800

    1.Add similarity of expected and actual.

--- nce2/nce2_01_a_private_conversation.py
+++ nce2/nce2_01_a_private_conversation.py
@@ -32,22 +32,23 @@ class TestStringMethods(unittest.TestCase):
                     "'This is a private conversation!'"
                     ]
 
-        actual = ["A private conversation.",
-                  "Why did the writer not to the man behind him?",
-                  "Last Sunday, I went to a theatre.",
-                  "I had a very good set.",
+        actual = ["A private conversation",
+                  "Why did the writer complain to the people behind him?",
+                  "Last week I went to the theatre.",
+                  "I had a very good seat.",
                   "The play was very interesting.",
                   "I did not enjoy it.",
                   "A young man and a young woman were sitting behind me.",
-                  "The talked loudly.",
+                  "They were talking loudly.",
+                  "I got very angry.",
                   "I could not hear the actors.",
                   "I turned round.",
                   "I looked at the man and the woman angrily.",
                   "They did not pay any attention.",
-                  "In the end, I could not enjoy it.",
+                  "In the end, I could not bear it.",
                   "I turned round again.",
-                  "'I cannot hear a word,' I said.",
-                  "'It's none of your business,' the man said rudely.",
+                  "'I can't hear a word,' I said angrily.",
+                  "'It's none of your business,' the young man said rudely.",
                   "'This is a private conversation!'"
                   ]
 
