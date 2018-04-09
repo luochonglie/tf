12ec046b5120990a583e5d10338f683fc4fb6989
Chad Luo <Chad.Luo@walmart.com>
2018-04-09 11:03:59 +0800

    1.Add nec 31 Success story.

--- nce2/nce2_31_success_story.py
+++ nce2/nce2_31_success_story.py
@@ -12,23 +12,37 @@ import Levenshtein as lvst
 
 class TestStringMethods(unittest.TestCase):
     def test_article(self):
-        expected = ["Success Story",
+        expected = ["Success story",
                     "What was Frank's first job?",
-                    "Yesterday afternoon Frank Hawkins was telling me about his experiences as young man.",
+                    "Yesterday afternoon Frank Hawkins was telling me about his experiences as a young man.",
                     "Before he retired, he was the head of a very large business company, "
-                    "but as a boy he used to work in a small shop."
-                    "It was his job to repair bicycles and at that time he used to work for fourteen hours a day.",
-                    "He save money for years and in 1958 he bought a small workshop for his own.",
+                    "but as a boy he used to work in a small shop.",
+                    "It was his job to repair bicycles and at that time he used to work fourteen hours a day.",
+                    "He saved money for years and in 1958 he bought a small workshop for his own.",
                     "In his twenties Frank used to make spare parts for aeroplanes.",
                     "At that time he had two helpers.",
-                    "In a few years his small workshop had become a big factory which employed seven hundred and "
-                    "twenty-eight people.",
+                    "In a few years his small workshop had become a large factory "
+                    "which employed seven hundred and twenty-eight people.",
                     "Frank smiled when he remembered his hard early years and the long road to success.",
                     "He was still smiling when the door opened and his wife came in.",
                     "She wanted him to repair their grandson's bicycle."
                     ]
 
-        actual = []
+        actual = ["Success story",
+                  "What was Frank's first job?",
+                  "Yesterday afternoon Frank Hawkins was telling me about his experiences as a young man.",
+                  "Before he retired, he was the head of a very large business company, "
+                  "but as a boy he used to work in a small shop.",
+                  "It was his job to repair bicycles and at that time he used to work fourteen hours a day.",
+                  "He saved money for years and in 1958 he bought a small workshop for his own.",
+                  "In his twenties Frank used to make spare parts for aeroplanes.",
+                  "At that time he had two helpers.",
+                  "In a few years his small workshop had become a large factory "
+                  "which employed seven hundred and twenty-eight people.",
+                  "Frank smiled when he remembered his hard early years and the long road to success.",
+                  "He was still smiling when the door opened and his wife came in.",
+                  "She wanted him to repair their grandson's bicycle."
+                  ]
 
         print("The similarity of two strings : %.2f%%" % (lvst.ratio("".join(expected), "".join(actual)) * 100))
 
