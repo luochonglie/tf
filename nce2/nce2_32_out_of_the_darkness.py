10e9706b659b217d3bd7e391b763fc47c61fc5f0
Chad Luo <Chad.Luo@walmart.com>
2018-05-14 15:20:39 +0800

    Add NCE 2-33 Out of the darkness.

---
+++ nce2/nce2_32_out_of_the_darkness.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/02/08 14:26
+# @Author  : c0l0121
+# @File    : nce2_27_a_wet_night.py
+# @Desc    :
+
+
+import unittest
+import Levenshtein as lvst
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_article(self):
+        expected = ["Out of the darkness",
+                    "Why was the girl in hospital?",
+                    "Nearly a week passed before the girl was able to explain what had happened to her.",
+                    "One afternoon she set out from the coast in a small boat and was caught in a storm.",
+                    "Towards evening, the boat struck a rock and the girl jumped into the sea.",
+                    "Then she swam to the shore after spending the whole night in the water.",
+                    "During that time she covered a distance of eight miles.",
+                    "Early next morning, she saw a light ahead.",
+                    "She knew she was near the shore because the light was high up on the cliffs.",
+                    "On arriving at the shore, the girl struggled up the cliff towards the light she had seen.",
+                    "That was all she remembered.",
+                    "When she woke up a day later, she found herself in hospital."
+                    ]
+
+        actual = ["Out of the darkness",
+                  "Why was the girl in hospital?",
+                  "Nearly a week passed before the girl was able to explain what had happened to her.",
+                  "One afternoon she set out from the coast in a small boat and was caught in a storm.",
+                  "Towards evening, the boat struck a rock and the girl jumped into the sea.",
+                  "Then she swam to the shore after spending the whole night in the water.",
+                  "During that time she covered a distance of eight miles.",
+                  "Early next morning, she saw a light ahead.",
+                  "She knew she was near the shore because the light was high up on the cliffs.",
+                  "On arriving at the shore, the girl struggled up the cliff towards the light she had seen.",
+                  "That was all she remembered.",
+                  "When she woke up a day later, she found herself in hospital."
+                  ]
+
+        print("The similarity of these articles : %.2f%%" % (lvst.ratio("".join(expected), "".join(actual)) * 100))
+
+        self.maxDiff = None
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
